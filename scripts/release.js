db159d364181386a5e60a33de5cd8428be7dd1b0
christian-dale <jchaukvik@gmail.com>
2022-05-01 18:57:01 +0200

    Use minify instead of minifier

--- scripts/release.js
+++ scripts/release.js
@@ -1,16 +1,10 @@
 "use strict";
 
 const fs = require('fs');
-const minifier = require('minifier');
 
 if (!fs.existsSync('./release')) {
 	fs.mkdirSync('./release');
 }
 
-minifier.on('error', (err) => {
-	console.error(err);
-});
-
-minifier.minify('./build/kernel.css', {output: './release/kernel.min.css'});
 fs.createReadStream('./build/kernel.css').pipe(fs.createWriteStream('./release/kernel.css'));
 fs.createReadStream('./js/kernel.js').pipe(fs.createWriteStream('./release/kernel.js'));
