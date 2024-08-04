fa391cb80cb667c0ad6923bcd9690db1320e8458
christian-dale <jchaukvik@gmail.com>
2024-08-04 16:23:46 +0200

    Simplify the build system

--- scripts/release.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const fs = require('fs');
-
-if (!fs.existsSync('./release')) {
-	fs.mkdirSync('./release');
-}
-
-fs.createReadStream('./build/kernel.css').pipe(fs.createWriteStream('./release/kernel.css'));
