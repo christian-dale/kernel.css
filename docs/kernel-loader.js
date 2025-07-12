6103aa0caac36dde4119594676a14b0d55252f53
christian-dale <jchaukvik@gmail.com>
2025-07-12 17:25:10 +0200

    Major overhaul of codebase

---
+++ docs/kernel-loader.js
@@ -0,0 +1,15 @@
+/**
+ * Load different kernel.css stylesheet and script based on the environment.
+ */
+
+const cssLink = document.createElement("link");
+cssLink.rel = "stylesheet";
+cssLink.type = "text/css";
+
+if (location.href.startsWith("http://localhost")) { // Development
+    cssLink.href = "/build/kernel.css";
+} else { // Production
+    cssLink.href = "https://cdn.jsdelivr.net/gh/Ionogy/kernel.css@1.0.0/build/kernel.min.css";
+}
+
+document.head.appendChild(cssLink);
