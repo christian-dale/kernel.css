c6b42ee5a1bcc2b1653855da83307da10363042c
christian-dale <jchaukvik@gmail.com>
2025-07-12 22:15:27 +0200

    Replace minify with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	plugins: [
-		require('postcss-prefix-selector')({
-			prefix: 'ke-',
-			transform: (prefix, selector, prefixedSelector) => {
+		require("postcss-prefix-selector")({
+			prefix: "ke-",
+			transform: (prefix, selector) => {
 				return selector.replace(/\.(\w[\w-]*)/g, `.${prefix}$1`);
 			}
 		})
